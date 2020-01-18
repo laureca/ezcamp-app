be9ae2a80829c2fae8457ec7106048e408e89a3f
laureca <anynnha.laureca@gmail.com>
2020-01-18 16:37:53 -0300

    inicializado o card da tela

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import {Image, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {Image, TouchableOpacity, View} from 'react-native';
 
 import logo from '../assets/images/logo.png';
 
-export default () => 
-    <View
-    style={{
-        flex: 1,
-        backgroundColor: '#E4E4E4',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-          <Image source={logo}/>
+export default ({navigation: {navigate}}) => {
+    const onLogout = useCallback(() => navigate('login'), [navigate]);
 
-    </View>;
+    return (
+        <View
+        style={{
+            flex: 1,
+            backgroundColor: '#E4E4E4',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+            <TouchableOpacity onPress={onLogout}>
+                <Image source={logo}/>
+            </TouchableOpacity>
+            <View
+            style={{
+                borderWidth: 1,
+                borderColor: '#ddd',
+                borderRadius: 25,
+                margin: 30,
+            }}>
+                <Image
+                style={{
+                    height: 300,
+                    width: 300,
+                }}
+                source={null}
+                />
+            </View>
+        </View>)};
 
